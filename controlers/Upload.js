34de323cb38d8e918e9e751c968e1ce32695aa2b
HumbertoCastroHorizonti <humberto.castro@horizonti.digital>
2024-10-11 13:11:19 -0300

    test

--- controlers/Upload.js
+++ controlers/Upload.js
@@ -1,45 +1,42 @@
-import AWS from 'aws-sdk';
-import fs from 'fs'
+import AWS from "aws-sdk";
+import fs from "fs";
 
 AWS.config.update({
-    accessKeyId: process.env.ACESS_KEY_AWS,
-    secretAccessKey: process.env.SECRET_ACESS_KEY_AWS,
-    region: 'us-east-1',
-  });
-  
+  accessKeyId: process.env.ACESS_KEY_AWS,
+  secretAccessKey: process.env.SECRET_ACESS_KEY_AWS,
+  region: "us-east-1",
+});
+
 // Criar uma instância do S3
 const s3 = new AWS.S3();
 
-
 // Upload file to S3 function
-export function uploadFileToS3(url, fileName) {
-    const fileStream = fs.createReadStream(url);
-  
-    const params = {
-      Bucket: 'consult-document-bucket',
-      Key: fileName,
-      Body: fileStream,
-      ContentType: 'application/pdf'
-    };
-  
-    return s3.upload(params).promise();
-  }
-  
-  // Generate presigned URL function
-export  function generatePresignedUrl(fileName) {
-    const params = {
-      Bucket: 'consult-document-bucket',
-      Key: fileName,
-      Expires: 604800,
-    };
-  
-    return new Promise((resolve, reject) => {
-      s3.getSignedUrl('getObject', params, (err, url) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(url);
-        }
-      });
+export const uploadFileToS3 = (file, fileName) => {
+  const params = {
+    Bucket: "consult-document-bucket",
+    Key: fileName,
+    Body: file.buffer,
+    ContentType: "application/pdf",
+  };
+
+  return s3.upload(params).promise();
+};
+
+// Generate presigned URL function
+export function generatePresignedUrl(fileName) {
+  const params = {
+    Bucket: "consult-document-bucket",
+    Key: fileName,
+    Expires: 604800,
+  };
+
+  return new Promise((resolve, reject) => {
+    s3.getSignedUrl("getObject", params, (err, url) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(url);
+      }
     });
-  }
+  });
+}
--- models/tbconsultas.js
+++ models/tbconsultas.js
@@ -1,56 +1,69 @@
-import DataTypes from 'sequelize';
-import sequelize from '../database.js';
-import StatusConsulta from './statusConsulta.js'; // Make sure this path is correct
+import DataTypes from "sequelize";
+import sequelize from "../database.js";
+import StatusConsulta from "./statusConsulta.js"; // Make sure this path is correct
 
-const Consultas = sequelize.define('tbconsultas', {
+const Consultas = sequelize.define(
+  "tbconsultas",
+  {
     id_consulta: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
     contact_id: {
-        type: DataTypes.STRING(50),
-        allowNull: false
+      type: DataTypes.STRING(50),
+      allowNull: false,
     },
     created_at: {
-        type: DataTypes.DATE,
-        allowNull: true,
-        defaultValue: DataTypes.NOW
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: DataTypes.NOW,
+    },
+    locked_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    locked: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
     },
     divida: {
-        type: DataTypes.STRING(50),
-        allowNull: true
+      type: DataTypes.STRING(50),
+      allowNull: true,
     },
     documento: {
-        type: DataTypes.STRING(50),
-        allowNull: true
+      type: DataTypes.STRING(50),
+      allowNull: true,
     },
     unidade: {
-        type: DataTypes.STRING(50),
-        allowNull: true
+      type: DataTypes.STRING(50),
+      allowNull: true,
     },
     id_ticket: {
-        type: DataTypes.STRING(50),
-        allowNull: false
+      type: DataTypes.STRING(50),
+      allowNull: false,
     },
-    status_id: { // Rename the field to reflect that it's a foreign key
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        references: {
-            model: StatusConsulta,
-            key: 'id'
-        }
+    status_id: {
+      // Rename the field to reflect that it's a foreign key
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: StatusConsulta,
+        key: "id",
+      },
     },
     url: {
-        type: DataTypes.STRING(255),  // Assuming URLs will not be longer than 255 characters
-        allowNull: true               // Assuming the URL is not mandatory
-    }
-}, {
-    tableName: 'tbconsultas',
-    timestamps: false
-});
+      type: DataTypes.STRING(255), // Assuming URLs will not be longer than 255 characters
+      allowNull: true, // Assuming the URL is not mandatory
+    },
+  },
+  {
+    tableName: "tbconsultas",
+    timestamps: false,
+  }
+);
 
 // Define the association
-Consultas.belongsTo(StatusConsulta, { foreignKey: 'status_id' });
+Consultas.belongsTo(StatusConsulta, { foreignKey: "status_id" });
 
 export default Consultas;
